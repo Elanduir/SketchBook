7d231627d18aaf6d6b0f5e6e5360c059e3177bc4
Fabian Heuberger <elanduir@gmail.com>
2023-05-12 14:46:41 +0200

    fixed apple pencil

--- src/modules/Handlers.js
+++ src/modules/Handlers.js
@@ -24,6 +24,13 @@ const setupHandlers = () => {
   canvas.addEventListener("pointerdown", startHandler);
   canvas.addEventListener("pointerup", endHandler);
   canvas.addEventListener("pointermove", moveHandler);
+  canvas.addEventListener("pointercancel", endHandler);
+  canvas.addEventListener("pointerleave", endHandler);
+  canvas.addEventListener("pointerout", endHandler);
+  canvas.addEventListener("touchstart", touchStartHandler);
+  canvas.addEventListener("touchend", touchEndHandler);
+  canvas.addEventListener("touchmove", touchMoveHandler);
+
   canvas.addEventListener("wheel", (event) => {
     let factor = event.deltaY < 0 ? 1.1 : 0.9;
     event.preventDefault();
@@ -53,9 +60,15 @@ const panHandler = (x, y) => {
 };
 
 const startHandler = (event) => {
-  event.preventDefault();
   activePointers.push(event);
   if (panPointerID === "") panPointerID = event.pointerId;
+  event.preventDefault();
+  if (
+    activePointers.length === 1 &&
+    (event.pointerType === "touch" || event.pointerType === "pen")
+  ) {
+    return;
+  }
 
   let x, y;
   [x, y] = getActualPositions(
@@ -72,7 +85,6 @@ const startHandler = (event) => {
       Math.pow(p1.clientX - p2.clientX, 2) +
         Math.pow(p1.clientY - p2.clientY, 2)
     );
-    console.log(distanceBetweenPointers);
   }
 
   panState = event.shiftKey;
@@ -101,6 +113,13 @@ const drawHandler = (x, y) => {
 };
 
 const moveHandler = (event) => {
+  if (
+    activePointers.length === 1 &&
+    (event.pointerType === "touch" || event.pointerType === "pen")
+  ) {
+    return;
+  }
+
   const SENSITIVITY = 5;
   let x, y;
   [x, y] = getActualPositions(event.clientX, event.clientY);
@@ -145,8 +164,45 @@ const moveHandler = (event) => {
   }
 };
 
+const touchStartHandler = (event) => {
+  event.preventDefault();
+  if (activePointers.length > 1) return;
+  let x, y;
+  [x, y] = getActualPositions(
+    event.changedTouches[0].clientX,
+    event.changedTouches[0].clientY
+  );
+  drawingState = true;
+  model.initCurrent(x, y);
+};
+
+const touchEndHandler = (event) => {
+  event.preventDefault();
+  if (activePointers.length > 1) return;
+  let x, y;
+  [x, y] = getActualPositions(
+    event.changedTouches[0].clientX,
+    event.changedTouches[0].clientY
+  );
+  model.endCurrent(x, y);
+  drawingState = false;
+};
+
+const touchMoveHandler = (event) => {
+  event.preventDefault();
+  if (activePointers.length > 1) return;
+  let x, y;
+  [x, y] = getActualPositions(
+    event.changedTouches[0].clientX,
+    event.changedTouches[0].clientY
+  );
+  drawHandler(x, y);
+};
+
 const handleZoom = (factor) => {
   model.manageZoom(factor);
 };
 
-const log = (text) => (dbg.innerHTML = text);
+const log = (text) => {
+  if (dbg) dbg.innerHTML = text;
+};
